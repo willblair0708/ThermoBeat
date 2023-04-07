67d9ce01ae897cbeec64ddcaaa6064a245033ea0
Will Blair <85643015+willblair0708@users.noreply.github.com>
2023-04-06 23:51:59 -0400

    Update simulation.py

--- software/simulation/src/simulation.py
+++ software/simulation/src/simulation.py
@@ -29,4 +29,50 @@ def ltc3108_output(input_voltage, input_current):
     Returns:
     tuple: A tuple containing the output voltage (in mV) and output current (in mA) for the given input voltage and current.
     """
-    # Constants for LTC
+    # Constants for LTC3108 simulation
+    output_voltage = 3300  # mV
+    efficiency = 0.8  # Assumed efficiency
+
+    input_power = input_voltage * input_current  # mW
+    output_power = input_power * efficiency  # mW
+    output_current = output_power / output_voltage  # mA
+
+    return output_voltage, output_current
+
+def simulate_temperature_gradients(temperature_gradients, open_circuit_voltage_per_gradient, short_circuit_current_per_gradient):
+    """
+    Simulate the system's behavior for various temperature gradients.
+
+    Args:
+    temperature_gradients (np.ndarray): An array containing the temperature gradient values.
+    open_circuit_voltage_per_gradient (float): The open-circuit voltage per degree Celsius of temperature gradient (in mV/°C).
+    short_circuit_current_per_gradient (float): The short-circuit current per degree Celsius of temperature gradient (in mA/°C).
+
+    Returns:
+    dict: A dictionary containing the simulation results (input voltage, input current, output voltage, and output current).
+    """
+    input_voltages = []
+    input_currents = []
+    output_voltages = []
+    output_currents = []
+
+    for gradient in temperature_gradients:
+        input_voltage, input_current = teg_output(gradient, open_circuit_voltage_per_gradient, short_circuit_current_per_gradient)
+        output_voltage, output_current = ltc3108_output(input_voltage, input_current)
+
+        input_voltages.append(input_voltage)
+        input_currents.append(input_current)
+        output_voltages.append(output_voltage)
+        output_currents.append(output_current)
+
+    results = {
+        'input_voltages': input_voltages,
+        'input_currents': input_currents,
+        'output_voltages': output_voltages,
+        'output_currents': output_currents,
+    }
+
+    return results
+
+def plot_simulation
+
