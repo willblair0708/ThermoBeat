dc1cd802fd7bb5b2b535c650db2ec4ab3a7fcfd2
Will Blair <85643015+willblair0708@users.noreply.github.com>
2023-04-07 00:27:54 -0400

    Update performance_plots.py

--- software/visualization/performance_plots.py
+++ software/visualization/performance_plots.py
@@ -3,6 +3,19 @@ import matplotlib.pyplot as plt
 import simulation
 import harvested_energy_estimation
 
+'''
+This script generates two plots:
+
+The first plot shows the relationship between TEG output voltage and output current with respect to the temperature gradient. It uses the teg_output function from the simulation module to calculate output voltage and output current for a range of temperature gradients.
+
+The second plot shows the relationship between harvested energy and duration, assuming a constant temperature gradient. It uses the energy_harvested function from the harvested energy estimation module to calculate the energy harvested for a range of durations.
+
+To generate the plots, execute the following command from the software/visualization folder:
+
+python performance_plots.py
+Two windows with the performance plots should appear, showing the relationship between TEG output, harvested energy, and the input parameters of temperature gradient and duration.
+'''
+
 def plot_teg_output_vs_temperature_gradient():
     temperature_gradients = np.linspace(1, 10, 100)
     output_voltages = []
